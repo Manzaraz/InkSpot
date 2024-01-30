495c65c897587b7011817b902d077e534f7bcf93
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-30 11:05:02 -0300

    Initial Commit

---
+++ InkSpot/InkSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkSpotApp.swift
+//  InkSpot
+//
+//  Created by Christian Manzaraz on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct InkSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
